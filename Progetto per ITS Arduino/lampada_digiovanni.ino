a90ad032298236386f54eafeb49b42c093acd50a
Davide Di Giovanni <davidedigiovanni999@gmail.com>
2020-10-08 18:42:02 +0200

    fix
    fix

--- Progetto per ITS Arduino/lampada_digiovanni.ino
+++ Progetto per ITS Arduino/lampada_digiovanni.ino
@@ -1,18 +1,34 @@
-#include <Adafruit_NeoPixel.h>
-#define LED_PIN 3
-#define LED_COUNT 20
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#include <Adafruit_NeoPixel.h> // Include for NeoPixel LED Ring
+#include <Wire.h>
+#include <Adafruit_GFX.h> // Include for display
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+   
+#define LED_PIN 3 // Where LED ring is connected
 
-/*##################################### CONF DI MQTT ######################################*/
+#define LED_COUNT 20 // How many LEDs in the ring
 
-#define BROKER_IP    "192.168.1.57"
+#define DHTPIN  2 // Where humi&temp sensor is connected
+
+#define DHTTYPE DHT11  // CHANGE THIS IF YOU ARE USING A DIFFERENT TYPE OF DHT SENSOR
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800); // DEFINE YOUR STRIP
+ 
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire); // Type of display used
+
+
+/*##################################### MQTT'S CONFIG ######################################*/
+
+#define BROKER_IP    "YOURIP"
 #define DEV_NAME     "mqttdevice"
 #define MQTT_USER    "test"
 #define MQTT_PW      "test"
-const char ssid[] = "TIM-19640501";
-const char pass[] = "gstSYZuwdfIaAwnVq5knVcaj";
+const char ssid[] = "YOURSSID"; //Change this to your SSID
+const char pass[] = "YOURPASS"; //Change this to your Wi-Fi password
 
-/*############################### FINE CONFIGUURAZIONE MQTT ####################*/
+/*############################### FINE CONFIGUURAZIONE MQTT #################################*/
+
+/*############################### WHERE WE DEFINE OUR BOARD ##########################*/
 
 #include <MQTT.h>
 #ifdef ARDUINO_SAMD_MKRWIFI1010
@@ -25,34 +41,46 @@ const char pass[] = "gstSYZuwdfIaAwnVq5knVcaj";
 #error unknown board
 #endif
 
+/*############################### END OF DEFINE  ################################*/
+
+DHT dht(DHTPIN, DHTTYPE);                // Configure DHT library
+ 
+char temperature[] = "00.0 C";
+char humidity[]    = "00.0 %";
+
+const int buttonPin = 0; // the number of the pushbutton pin
+int buttonState = 0; // button state
+
 WiFiClient net;
 MQTTClient client;
 unsigned long lastMillis = 0;
 
-const int buttonPin = 2; // the number of the pushbutton pin
-int buttonState = 0; //stato bottone
+int mode = 0; // Mode of the lamp
+int brightness = 50; // Brightness
+String payload2; // I use this to copy the payload for later use
 
-int mode = 0; // Questa variabile controlla il colore
-int brightness = 50; // Inizializzo luminosita' a circa 1/3
-String payload2; // Mi serve per l'rgb
 
-/* ##################### Funzioni connessione  ############################*/
+
+/* ##################### Wi-Fi Functions ############################*/
 
 void connect() {
-  Serial.print("Controllo Wi-Fi...");
+  Serial.print("Checking Wi-Fi...");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
-  Serial.print("\nConnessione alla rete...");
+  Serial.print("\nConnecting to MQTT broker");
   while (!client.connect(DEV_NAME, MQTT_USER, MQTT_PW)) {
     Serial.print(".");
     delay(1000);
   }
-  Serial.println("\nCollegato correttamente!");
   client.subscribe("/controllo"); //SUBSCRIBE TO TOPIC /controllo
-  Serial.println("\nTopic collegato!");
+  client.subscribe("/readtemp"); //SUBSCRIBE TO TOPIC /readtemp
+  client.subscribe("/readhumi"); //SUBSCRIBE TO TOPIC /readhumi
+  Serial.println("\nConnection is good! Yay!");
 }
+
+/* ##################### End Wi-Fi Functions ############################*/
 
 void messageReceived(String &topic, String &payload) {
   Serial.println("\nRicevo: " + payload);
@@ -104,20 +132,25 @@ void setup() {
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(brightness); // Set BRIGHTNESS
   Serial.begin(115200);
-  Serial.println("\nAvvio/Reset lampada");
   WiFi.begin(ssid, pass);
   client.begin(BROKER_IP, 1883, net);
   client.onMessage(messageReceived);
   mode = 4; //Inizialize this at WHITE
   connect();
+  Serial.println("\nBooting completed!");
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.display();
+  dht.begin();                // Initialize the DHT library
+  display.clearDisplay();
+  display.drawFastHLine(0, 32, SSD1306_LCDWIDTH, WHITE);
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
 }
 
 void loop() {
   client.loop();
-  if (buttonState == HIGH) {
   client.onMessage(messageReceived);
-  }
-  switch (mode) {          // Start the new animation...
+  switch (mode) {          
     case 0:
       colorWipe(strip.Color(  0,   0,   0));    // Black/off
       break;
@@ -136,7 +169,7 @@ void loop() {
     case 5:
       String hexstring = payload2;
       Serial.println("DEBUG HEX:" + hexstring);
-      unsigned long int number = (long) strtol( &hexstring[1], NULL, 16);
+      unsigned long int number = (long) strtol( &hexstring[0], NULL, 16);
       int red = number >> 16;
       int green = number >> 8 & 0xFF;
       int blue = number & 0xFF;
@@ -144,6 +177,23 @@ void loop() {
       colorWipe(strip.Color(  red,  green, blue));  //User defined colour
       break;
   }
+  byte RH = dht.readHumidity();
+  //Read temperature in degree Celsius
+  byte Temp = dht.readTemperature();
+  display.setCursor(0,0); 
+  display.clearDisplay();
+  temperature[0] = Temp / 10 + 48;
+  temperature[1] = Temp % 10 + 48;
+  humidity[0]    = RH / 10 + 48;
+  humidity[1]    = RH % 10 + 48;
+  display.print("T: ");
+  display.print(temperature);
+  display.print("\nH: ");
+  display.drawFastHLine(0, 32, SSD1306_LCDWIDTH, WHITE);
+  display.print(humidity);
+  display.display();
+  client.publish("/datitemp", temperature);
+  client.publish("/datihum", humidity);
 }
 
 
